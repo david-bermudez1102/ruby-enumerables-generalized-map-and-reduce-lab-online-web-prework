33426930a6241cd9a3f05f8a9a34ac03698824d2
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-22 22:56:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,7 @@ def reduce(source_array, starting_point ="0")
   total = starting_point
 
   while i < source_array.length do
-    yield(source_array[i],total)
+    total = yield(source_array[i],total)
     i += 1
   end
 
