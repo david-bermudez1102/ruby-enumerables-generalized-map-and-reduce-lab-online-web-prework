c036928558e3d9e8e247e3d672054472fd401f33
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-22 21:43:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,13 @@
 # Your Code Here
 def map(array)
- yield(array)
+  new = []
+    i = 0
+    while i < source_array.length do
+      yield new.push( source_array[i] * -1 ) 
+      i += 1
+    end
+    return new
+
 end
 
 
