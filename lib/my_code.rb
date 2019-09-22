d76bc7f64f800abb16b97f900d59c26656166f93
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-22 22:55:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,12 +9,12 @@ def map(source_array)
 end
 
 
-def reduce(source_array, starting_point)
+def reduce(source_array, starting_point ="0")
   i = 0
-  total = 0
+  total = starting_point
 
   while i < source_array.length do
-    yield(source_array[i])
+    yield(source_array[i],total)
     i += 1
   end
 
